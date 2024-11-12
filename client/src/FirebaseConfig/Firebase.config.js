93b75507001ba4425f22fc750444b7f28c22c3fe
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-12 19:05:58 +0600

    react router dom

--- client/src/FirebaseConfig/Firebase.config.js
+++ client/src/FirebaseConfig/Firebase.config.js
@@ -1,6 +1,3 @@
-// Your firebase project items
-// add your domain here(vercel custom domain)
-// make firebase api key secure
 // install react router dom
 // do as like as before
 // Import the functions you need from the SDKs you need
