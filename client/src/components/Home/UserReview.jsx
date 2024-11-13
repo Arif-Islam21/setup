882256e5c3a5cc74d2ead2f6968f6d440e6f2f03
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-13 23:15:13 +0600

    user review section added

---
+++ client/src/components/Home/UserReview.jsx
@@ -0,0 +1,14 @@
+import Review from "./Review";
+
+const UserReview = () => {
+  return (
+    <div className="grid grid-cols-4 gap-4 mx-4">
+      <Review />
+      <Review />
+      <Review />
+      <Review />
+    </div>
+  );
+};
+
+export default UserReview;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Home/Banner";
 import FeaturedProducts from "../../components/Home/FeaturedProducts";
+import UserReview from "../../components/Home/UserReview";
 
 const Home = () => {
   return (
@@ -10,6 +11,10 @@ const Home = () => {
           Featured Products
         </h1>
         <FeaturedProducts />
+      </div>
+      <div className="my-8">
+        <h1 className="mb-8 text-2xl font-semibold text-center">User Review</h1>
+        <UserReview />
       </div>
     </div>
   );
