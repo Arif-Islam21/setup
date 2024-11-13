f0a16c11703c722c1b6b5ba9592ee1b41977ae75
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-13 23:11:16 +0600

    card done

--- client/src/components/Home/FeaturedProducts.jsx
+++ client/src/components/Home/FeaturedProducts.jsx
@@ -1,5 +1,14 @@
+import ProductCard from "../ProductCard";
+
 const FeaturedProducts = () => {
-  return <div>Featured Products</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 mx-4">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
 };
 
 export default FeaturedProducts;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import Banner from "../../components/Home/Banner";
+import FeaturedProducts from "../../components/Home/FeaturedProducts";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <div className="my-8">
+        <h1 className="mb-8 text-2xl font-semibold text-center">
+          Featured Products
+        </h1>
+        <FeaturedProducts />
+      </div>
     </div>
   );
 };
