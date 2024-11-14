1ab1f83efc164148cd205393ac673c9741231f55
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-14 09:25:10 +0600

    navbar customized

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import About from "../pages/About";
+import Products from "../pages/Products";
+import Contact from "../pages/Contact";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/contacts",
+        element: <Contact />,
       },
     ],
   },
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-200">
@@ -27,48 +29,37 @@ const Navbar = () => {
               <a>Item 1</a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
               <a>Item 3</a>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Gadget Shop</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu gap-2 menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <NavLink to={"/"}>Home</NavLink>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <NavLink to={"/products"}>Products</NavLink>
           </li>
           <li>
-            <a>Item 3</a>
+            <NavLink to={"/about"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contacts"}>Contact Us</NavLink>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className="flex gap-2 items-center">
+          <a className="btn bg-gray-400 rounded-md items-center text-black border border-black btn-sm">
+            Sign In
+          </a>
+          <a className="btn bg-black rounded-md items-center text-white btn-sm">
+            Sign Up
+          </a>
+        </div>
       </div>
     </div>
   );
