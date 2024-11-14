97fc8e855f92d57fc1b7616661561f7727e55019
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-14 09:57:02 +0600

    useing auth provider

---
+++ client/src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+import { app } from "../FirebaseConfig/Firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = () => {
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authInfo = {
+    createUser,
+    login,
+    logOut,
+    googleLogin,
+  };
+
+  return <div></div>;
+};
+
+export default AuthProvider;
