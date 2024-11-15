cf3b674e72c366bc03eb66cdb767eae492bdb706
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-15 12:46:29 +0600

    two middleware added to add product post

--- server/index.js
+++ server/index.js
@@ -11,6 +11,33 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+// TOKEN VERIFICATION
+const verifyToken = (req, res, next) => {
+  const authorization = req.header.authorization;
+  if (!authorization) {
+    return res.send({ message: "No Token Found" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// VERIFY SELLER
+const verifySeller = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbidden access" });
+  }
+  next();
+};
+
 // MONGODB
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.knlt5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -58,6 +85,13 @@ async function dbConnect() {
         return res.send({ message: "user already exists" });
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // ADD PRODUCTS
+    app.post("/add-products", verifyToken, verifySeller, async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
       res.send(result);
     });
 
