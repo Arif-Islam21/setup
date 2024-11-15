55b9df1608cd2299c539004d014bbaec1cef6469
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-15 11:36:25 +0600

    sidebar done

--- client/src/components/Dashboard/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { GrOverview } from "react-icons/gr";
+import { IoHomeOutline, IoLogOutOutline } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
     <div className="bg-gray-200 px-8 pt-4  border-r-2 min-h-screen">
       <h1 className="text-2xl font-bold mb-8">Gadget Shop</h1>
       <ul className="flex flex-col gap-2">
-        <li className="btn">
+        <li className="py-2 px-4 text-start border-2 rounded-md border-gray-300 bg-white hover:bg-black/10 transition-colors delay-150">
           <NavLink
             className={"flex items-center gap-3 text-xl font-bold"}
             to={"/dashboard/overview"}
@@ -15,11 +16,20 @@ const Sidebar = () => {
             Overview
           </NavLink>
         </li>
-        <li>
-          <NavLink to={"/"}>Home</NavLink>
+        <li className="py-2 px-4 text-start border-2 rounded-md border-gray-300 bg-white hover:bg-black/10 transition-colors delay-150">
+          <NavLink
+            className={"flex items-center gap-3 text-xl font-bold"}
+            to={"/"}
+          >
+            <IoHomeOutline />
+            Home
+          </NavLink>
         </li>
-        <li>
-          <button>Logout</button>
+        <li className="py-2 px-4 text-start border-2 rounded-md border-gray-300 bg-white hover:bg-black/10 transition-colors delay-150">
+          <button className={"flex items-center gap-3 text-xl font-bold"}>
+            <IoLogOutOutline />
+            Logout
+          </button>
         </li>
       </ul>
     </div>
