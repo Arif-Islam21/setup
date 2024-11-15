f13cb8e285575be55868d17d6faa0d5205d74ec9
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-15 15:15:23 +0600

    design done

--- client/src/pages/Dashboard/Seller/AddProducts.jsx
+++ client/src/pages/Dashboard/Seller/AddProducts.jsx
@@ -36,20 +36,99 @@ const AddProducts = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Email</span>
+                <span className="label-text">Brand</span>
               </label>
               <input
                 type="text"
-                placeholder="Title"
+                placeholder="Brand"
                 className="input input-bordered"
-                {...register("title", { required: true })}
+                {...register("brand", { required: true })}
               />
-              {errors.title && (
+              {errors.brand && (
                 <p className="text-red-500 font-bold">
-                  Title Field Is Required
+                  Brand Field Is Required
                 </p>
               )}
             </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Price"
+                className="input input-bordered"
+                {...register("price", { required: true })}
+              />
+              {errors.price && (
+                <p className="text-red-500 font-bold">
+                  price Field Is Required
+                </p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Stock</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Stock"
+                className="input input-bordered"
+                {...register("Stock", { required: true })}
+              />
+              {errors.Stock && (
+                <p className="text-red-500 font-bold">
+                  Stock Field Is Required
+                </p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Category</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Category"
+                className="input input-bordered"
+                {...register("Category", { required: true })}
+              />
+              {errors.Category && (
+                <p className="text-red-500 font-bold">
+                  Category Field Is Required
+                </p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Seller Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Seller Email"
+                className="input input-bordered"
+                {...register("SellerEmail", { required: true })}
+              />
+              {errors.SellerEmail && (
+                <p className="text-red-500 font-bold">
+                  SellerEmail Field Is Required
+                </p>
+              )}
+            </div>
+            <label className="form-control col-span-2">
+              <div className="label">
+                <span className="label-text">Description</span>
+              </div>
+              <textarea
+                className="textarea textarea-bordered h-24"
+                placeholder="Description"
+                {...register("description", { required: true })}
+              ></textarea>
+              {errors.description && (
+                <p className="text-red-500 font-bold">
+                  Description Field Is Required
+                </p>
+              )}
+            </label>
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-primary">Register</button>
