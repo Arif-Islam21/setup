c3fbefae6f34515d7f948e3efdf6d7cb51ddb886
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-25 17:18:33 +0600

    lets implement sorting with react query

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -56,14 +56,15 @@ const ProductCard = ({ product, isInWishlist, setLatestData }) => {
           // src="https://img.freepik.com/premium-photo/virtual-reality-glasses-tablet-with-headphones-gray-background-top-view_157927-7597.jpg?w=826"
           src={product?.photoURL}
           alt={product?.brand}
-          className="h-40 object-cover w-full"
+          className="h-40 max-h-40 object-cover w-full"
         />
       </figure>
       <div className="card-body">
         <h2 className="text-xl font-semibold uppercase">{product?.title}</h2>
         <div className="flex justify-between ">
           <p className="font-semibold flex items-center gap-2">
-            <TbBrandAppgallery size={20} /> {product?.brand}
+            <TbBrandAppgallery size={20} />
+            {product?.brand}
           </p>
           <p className="justify-end flex items-center gap-2">
             <IoPricetagsOutline size={20} /> {product?.priceInt}
